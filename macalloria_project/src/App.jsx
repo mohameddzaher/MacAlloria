0890fcd0a1a860cf45231d5fc5cc1e291c5d3136
mohamed zaher <mohamedzaher.dev@gmail.com>
2025-10-02 23:22:23 +0300

    Changed app.js from BrowserRouter to HashRouter

--- macalloria_project/src/App.jsx
+++ macalloria_project/src/App.jsx
@@ -1,51 +1,78 @@
 // import React from "react";
-// import Header from "./components/Header";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import WhatsAppButton from "./components/WhatsAppButton";
+
+// // صفحات الهوم
 // import HeroSection from "./components/HeroSection";
 // import FeaturedProducts from "./components/FeaturedProducts";
 // import WhyChooseUs from "./components/WhyChooseUs";
-// import Footer from "./components/Footer";
-// import Map from "./components/Map";
-// import ContactForm from "./components/ContactForm";
-// import NewsletterForm from "./components/NewsletterForm";
 // import AboutSection from "./components/AboutSection";
 // import Testimonials from "./components/Testimonials";
 // import FAQSection from "./components/FAQSection";
+// import ContactForm from "./components/ContactForm";
+// import Map from "./components/Map";
+// import NewsletterForm from "./components/NewsletterForm";
 // import FeatureHighlight from "./components/FeatureHighlight";
 // import StatsCounter from "./components/StatsCounter";
-// import WhatsAppButton from "./components/WhatsAppButton";
+// import ScrollToTop from "./components/ScrollToTop";
 
+// // صفحات المنتجات
+// import ProductsPage from "./pages/ProductsPage";
+// import CategoryPage from "./pages/CategoryPage";
+// import ProductDetails from "./pages/ProductDetails";
+// import ContactPage from "./pages/ContactPage";
+// import AboutPage from "./pages/AboutPage";
 
-
-// AboutSection;
-// function App() {
+// function HomePage() {
 //   return (
 //     <>
-//       <Header />
 //       <HeroSection />
 //       <StatsCounter />
-      
 //       <FeaturedProducts />
 //       <WhyChooseUs />
 //       <FeatureHighlight />
-      
-      
 //       <Testimonials />
 //       <AboutSection />
 //       <FAQSection />
-//       <ContactForm />
+//       <ContactForm className="my-20" />
 //       <Map />
 //       <NewsletterForm />
-//       <WhatsAppButton />
-//       <Footer />
+      
 //     </>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <Router>
+//       <ScrollToTop /> 
+//       <Header />
+//       <WhatsAppButton />
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/products" element={<ProductsPage />} />
+//         <Route path="/products/:category" element={<CategoryPage />} />
+//         <Route path="/product/:id" element={<ProductDetails />} />
+//         <Route path="/contact" element={<ContactPage />} />
+//         <Route path="/about" element={<AboutPage />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
 //   );
 // }
 
 // export default App;
 
+
+
+
+
+
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -86,7 +113,6 @@ function HomePage() {
       <ContactForm className="my-20" />
       <Map />
       <NewsletterForm />
-      
     </>
   );
 }
