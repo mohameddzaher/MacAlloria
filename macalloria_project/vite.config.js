331c28445a0aeb732cd3dce208def36b9d43547d
mohamed zaher <mohamedzaher.dev@gmail.com>
2025-10-09 22:25:40 +0300

    fix vite base path for production

--- macalloria_project/vite.config.js
+++ macalloria_project/vite.config.js
@@ -1,19 +1,9 @@
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss()
-//   ],
-// })
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/MacAlloria/',
+  base: '/', // 👈 لازم كده بالضبط
   plugins: [
     react(),
     tailwindcss()
